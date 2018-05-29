0936699545e70c6c04c7e0f4389d3456a685a729
Andrew Alferenkov <alferenkovandrei3373@gmail.com>
2018-05-29 16:13:22 +0300

    fix namespace

--- tests/ServiceProviderRunnerTest.php
+++ tests/ServiceProviderRunnerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Andrewalf\ServiceProviders\Tests;
 
 use Andrewalf\Exceptions\ServiceProviderClassNotFound;
 use Andrewalf\Exceptions\ServiceProviderContractNotImplementedException;
